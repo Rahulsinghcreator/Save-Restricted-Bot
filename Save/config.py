a67ebb5759f617e50b012f242813f99d3411cffb
arnobpaul <117028354+arnobpaul@users.noreply.github.com>
2024-05-22 07:17:08 +0530

    Update config.py

--- Save/config.py
+++ Save/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
      
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = os.environ.get("API_ID", "")
+    API_ID = int(os.environ.get("API_ID", 26921911))
     API_HASH = os.environ.get("API_HASH", "")
     STRING = os.environ.get("STRING", "")
     
