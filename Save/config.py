9f696a373bb0c7574afdc37f15f23d97a9fe7918
Rahulsinghcreator <138279223+Rahulsinghcreator@users.noreply.github.com>
2024-05-27 08:20:52 +0530

    Update config.py

--- Save/config.py
+++ Save/config.py
@@ -2,9 +2,9 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 26921911))
-    API_HASH = os.environ.get("API_HASH", "")
-    STRING = os.environ.get("STRING", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7118086758:AAEhE-DY6GUqVm4Ews6OfLqqvLldVuRo7m4")
+    API_ID = int(os.environ.get("API_ID", 25274255))
+    API_HASH = os.environ.get("API_HASH", "02b2832c0e2cc02d8cf178c8a95a5d59")
+    STRING = os.environ.get("STRING", "AQGqa00Awft9PN8hoYwpWfcY7aFWDX7CfsAoret2pKfsiMib2cPwH7eflRYryxxf1BNIjznY4ubs-yAgeJ3ZqH4RHjt2j1usgERor3Zn8mQk6_M3wnVkUr9jXdiQwKlx96i-nihrUuGLsPdo_TALhJW8l53VD71yE0GWdDH1wepXLqP1f4JWJ7FI1xEQ6GKxdpiF9tYIYYzs8iMYckmq4IOEa6b8qh6EWx9W4FKN2AGpiaZqECb0OOAWgGFz6XBlh8XXk8XAF_Ws0Cl8l6kirGLWawm9ZBmuF4DFUPQveImnXPUybdhATi5PQ-2P_y75L43YW7N6wT5GLp8Kg9aPPrDBJFpcegAAAAGnbPQIAA")
     
     
